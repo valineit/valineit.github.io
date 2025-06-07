3374b725941aaaebfaeaf7e8d3fc1c213d51356f
valineit <valineitgoli@gmail.com>
2025-06-07 16:00:19 +0900

    a

--- chapter.js
+++ chapter.js
@@ -1,3 +1,6 @@
+
+
+//ページとチャプターの初期値を調べる
 const params = new URLSearchParams(window.location.search);
 
 const openFile = params.get('p');
@@ -19,6 +22,30 @@ async function chapter(chapterFile,chapterId){
     const extracted=doc.querySelector(`[id="${chapterId}"]`);
     if (extracted) {
       document.getElementById("content").innerHTML=extracted.innerHTML;
+    } else {
+      console.error("指定の要素が見つかりませんでした");
+    }
+  } catch (err) {
+    console.error("読み込みエラー:", err);
+  }
+
+}
+
+async function pageOpen(chapterFile,chapterId){
+  try {
+    const res = await fetch(`chapters/${chapterFile}.html`);
+    const htmlText = await res.text();
+
+    const parser=new DOMParser();
+    const doc=parser.parseFromString(htmlText,'text/html');
+
+    const extracted=doc.querySelector(`[id="${chapterId}"]`);
+    const links=doc.querySelector(`[id="links"]`)
+    const h1=doc.querySelector(`[id="h1"]`)
+    if (extracted,links) {
+      document.getElementById("content").innerHTML=extracted.innerHTML;
+      document.getElementById("links").innerHTML=links.innerHTML;
+      document.getElementById("h1").innerHTML=h1.innerHTML;
     } else {
       console.error("指定の要素が見つかりませんでした");
     }
